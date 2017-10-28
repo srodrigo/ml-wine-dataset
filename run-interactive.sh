670b44cc5ed50f36b45d0bdc0a4bd98573f04e35
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-10-28 15:59:58 +0100

    add script for running in non-interactive mode

--- run-interactive.sh
+++ run-interactive.sh
@@ -7,6 +7,5 @@ docker run \
   -v ${PWD}/src:/app/src:ro \
   -v ${PWD}/data:/app/data \
   -v ${PWD}/graphs:/app/graphs \
-  wine-dataset \
-  python3 src/main/main.py ./data/wine.data ./graphs
+  -it wine-dataset
 
--- src/main/main.py
+++ src/main/main.py
@@ -1,6 +1,34 @@
 #!/bin/python
+import pandas as pd
+import seaborn as sns
+import argparse
 
 
-if __name__ == '__main__':
-    print('hello world')
+parser = argparse.ArgumentParser(
+        description='Provide input data file and graphs folder')
+parser.add_argument('input_data_file', type=str, help='input data file')
+parser.add_argument('graphs_folder', type=str, help='graphs folder')
+args = parser.parse_args()
 
+columns = [
+    'Unknown',
+    'Alcohol',
+    'Malic acid',
+    'Ash',
+    'Alcalinity of ash',
+    'Magnesium',
+    'Total phenols',
+    'Flavanoids',
+    'Nonflavanoid phenols',
+    'Proanthocyanins',
+    'Color intensity',
+    'Hue',
+    'OD280/OD315 of diluted wines',
+    'Proline'
+]
+
+print('Loading data...')
+wine_data = pd.read_csv(args.input_data_file, names=columns)
+print('Generating pair plot...')
+wine_pairplot = sns.pairplot(wine_data)
+wine_pairplot.savefig('./graphs/wine.png')
