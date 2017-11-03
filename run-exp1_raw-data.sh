18b220f058e30de1895ba54706f8fc067a391e84
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-11-03 23:19:35 +0000

    plot cross-validation results

--- run-exp1_raw-data.sh
+++ run-exp1_raw-data.sh
@@ -4,6 +4,7 @@ docker build -t wine-dataset .
 docker run \
   -v ${PWD}/src:/app/src:ro \
   -v ${PWD}/data:/app/data \
+  -v ${PWD}/graphs:/app/graphs \
   wine-dataset \
-  python3 src/main/exp1_raw-data.py ./data/wine.data
+  python3 src/main/exp1_raw-data.py ./data/wine.data ./graphs/
 
--- src/main/exp1_raw-data.py
+++ src/main/exp1_raw-data.py
@@ -4,6 +4,7 @@ import dataset
 import argparse
 
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -16,6 +17,7 @@ from sklearn.svm import SVC
 parser = argparse.ArgumentParser(
         description='Provide input data file')
 parser.add_argument('input_data_file', type=str, help='input data file')
+parser.add_argument('graphs_folder', type=str, help='graphs folder')
 args = parser.parse_args()
 
 wine_data = pd.read_csv(args.input_data_file, names=dataset.COLUMNS)
@@ -29,19 +31,32 @@ X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.20, random_state=SEED)
 
 MODELS = (
-    ('Logistic Regression', LogisticRegression()),
-    ('Linear Discriminant Analysis', LinearDiscriminantAnalysis()),
-    ('KNeighbors', KNeighborsClassifier()),
-    ('Decision Tree', DecisionTreeClassifier()),
-    ('Gaussian NB', GaussianNB()),
+    ('LR', LogisticRegression()),
+    ('LDA', LinearDiscriminantAnalysis()),
+    ('KN', KNeighborsClassifier()),
+    ('CART', DecisionTreeClassifier()),
+    ('NB', GaussianNB()),
     ('SVM', SVC())
 )
 
 print('Evaluating models...')
 results = []
-for desc, model in MODELS:
+names = []
+for name, model in MODELS:
+    names.append(name)
+
     kfold = KFold(n_splits=10, random_state=SEED)
     cv_score = cross_val_score(
             model, X_train, y_train, cv=kfold, scoring='accuracy')
     results.append(cv_score)
-    print("%s: %f (%f)" % (desc, cv_score.mean(), cv_score.std()))
+    print("%s: %f (%f)" % (name, cv_score.mean(), cv_score.std()))
+
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+              alpha=0.5)
+plt.show()
+plt.savefig(args.graphs_folder + 'exp1_raw-data_alg-comparison.png')
