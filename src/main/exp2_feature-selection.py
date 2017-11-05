92405e41309eef3c248dce755be8ba71fc11b7e2
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-11-05 22:37:09 +0000

    add feature selection experiment

---
+++ src/main/exp2_feature-selection.py
@@ -0,0 +1,58 @@
+#!/bin/python
+import dataset
+
+import argparse
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+parser = argparse.ArgumentParser(
+        description='Provide input data file')
+parser.add_argument('input_data_file', type=str, help='input data file')
+parser.add_argument('graphs_folder', type=str, help='graphs folder')
+args = parser.parse_args()
+
+wine_data = pd.read_csv(args.input_data_file, names=dataset.COLUMNS)
+
+SEED = 23
+
+X = wine_data.drop('Class', axis=1)
+y = wine_data['Class']
+
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.20, random_state=SEED)
+
+MODELS = (
+    ('LR', LogisticRegression()),
+    ('LDA', LinearDiscriminantAnalysis()),
+    ('KN', KNeighborsClassifier()),
+    ('CART', DecisionTreeClassifier()),
+    ('NB', GaussianNB()),
+    ('SVM', SVC())
+)
+
+print('\nEvaluating models...')
+
+best_features = SelectKBest(chi2, k=3)
+X_train = best_features.fit_transform(X_train, y_train)
+X_test = best_features.transform(X_test)
+
+results = []
+names = []
+for name, model in MODELS:
+    print("\nMaking predictions for %s..." % name)
+
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    print("Accuracy: %f" % accuracy_score(y_test, predictions))
+    print(confusion_matrix(y_test, predictions))
+    print(classification_report(y_test, predictions))
