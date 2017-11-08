e08d7d90e4cb7e411b858cd895004fea16b85bce
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-11-08 19:37:50 +0000

    store results into a file

--- src/main/exp1_raw-data.py
+++ src/main/exp1_raw-data.py
@@ -18,6 +18,7 @@ parser = argparse.ArgumentParser(
         description='Provide input data file')
 parser.add_argument('input_data_file', type=str, help='input data file')
 parser.add_argument('graphs_folder', type=str, help='graphs folder')
+parser.add_argument('results_folder', type=str, help='results folder')
 args = parser.parse_args()
 
 wine_data = pd.read_csv(args.input_data_file, names=dataset.COLUMNS)
@@ -41,11 +42,29 @@ MODELS = (
 
 print('\nEvaluating models...')
 results = []
-names = []
 for name, model in MODELS:
     print("\nMaking predictions with %s..." % name)
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
-    print("Accuracy: %f" % accuracy_score(y_test, predictions))
-    print(confusion_matrix(y_test, predictions))
-    print(classification_report(y_test, predictions))
+
+    acc_score = accuracy_score(y_test, predictions)
+    conf_matrix = confusion_matrix(y_test, predictions)
+    class_report = classification_report(y_test, predictions)
+    results.append({
+        'model_name': name,
+        'acc_score': acc_score,
+        'conf_matrix': conf_matrix,
+        'class_report': class_report
+    })
+
+    print("Accuracy: %f" % acc_score)
+    print(conf_matrix)
+    print(class_report)
+
+results_file_name = args.results_folder + 'exp1_raw-data_results.txt'
+with open(results_file_name, 'w') as results_file:
+    for res in results:
+        results_file.write("Model: %s\n" % res['model_name'])
+        results_file.write("Accuracy: %f\n" % res['acc_score'])
+        results_file.write(str(res['conf_matrix']) + "\n")
+        results_file.write(res['class_report'] + "\n")
