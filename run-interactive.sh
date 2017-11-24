5ef8df160fd1e45a54926224afb6d7cffdf651d7
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-11-24 10:10:35 +0000

    standarise run scripts

--- run-interactive.sh
+++ run-interactive.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-mkdir -p graphs
+data_dir='data'
+graphs_dir='graphs'
+results_dir='results'
+
+mkdir -p ./${graphs_dir}
+mkdir -p ./${results_dir}
 
 docker build -t wine-dataset .
 docker run \
   -v ${PWD}/src:/app/src:ro \
-  -v ${PWD}/data:/app/data \
-  -v ${PWD}/graphs:/app/graphs \
+  -v ${PWD}/${data_dir}:/app/${data_dir} \
+  -v ${PWD}/${graphs_dir}:/app/${graphs_dir} \
+  -v ${PWD}/${results_dir}:/app/${results_dir} \
   -it wine-dataset
 
