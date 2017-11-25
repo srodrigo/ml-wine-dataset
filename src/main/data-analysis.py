6175a17df63791d037dd82dd9b20cdc89297e8c3
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-11-25 10:33:33 +0000

    refactor: extract common graphs functions

--- src/main/data-analysis.py
+++ src/main/data-analysis.py
@@ -1,10 +1,12 @@
 #!/bin/python
 import dataset
 import inputs
+from graphs import boxplot
+from graphs import hist
+from graphs import hist_by
+from graphs import pairplot
 
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 
 args = inputs.parse_args()
@@ -22,26 +24,28 @@ print('\nStatistics')
 print(wine_data.describe())
 
 print('\nGenerating box plot...')
-plt.figure(figsize=(75, 30))
-wine_boxplot = sns.boxplot(data=wine_data)
-plt.show()
-plt.savefig(args.graphs_folder + 'wine-boxplot.png')
+boxplot(
+    data=wine_data,
+    figsize=(75, 30),
+    file_name=args.graphs_folder + 'wine-boxplot.png')
 
 print('\nGenerating dist plot...')
-wine_data.hist(bins=30, figsize=(16, 12))
-plt.show()
-plt.savefig(args.graphs_folder + 'wine-hist.png')
+hist(
+    data=wine_data,
+    bins=30,
+    figsize=(16, 12),
+    file_name=args.graphs_folder + 'wine-hist.png')
 
 print('\nGenerating dist plot grouped by class...')
-wine_hist_by_class = wine_data.groupby('Class').hist(bins=30, figsize=(16, 12))
-for wine_class, wine_hist in zip((1, 2, 3), wine_hist_by_class):
-    fig = wine_hist[0, 0].get_figure()
-    plt.title('Class: %d' % wine_class, fontsize=20)
-    fig.suptitle('Class: %d' % wine_class, fontsize=20)
-    fig.savefig('%swine-hist-class-%d.png' % (args.graphs_folder, wine_class))
-    plt.close()
+hist_by(
+    data=wine_data,
+    by='Class',
+    by_values=(1, 2, 3),
+    bins=30,
+    figsize=(16, 12),
+    folder_name=args.graphs_folder)
 
 print('\nGenerating pair plot...')
-wine_pairplot = sns.pairplot(wine_data)
-wine_pairplot.savefig(args.graphs_folder + 'wine-pairplot.png')
-plt.show()
+pairplot(
+    data=wine_data,
+    file_name=args.graphs_folder + 'wine-pairplot.png')
---
+++ src/main/graphs.py
@@ -0,0 +1,44 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def boxplot(data, figsize=(25, 10), file_name=None):
+    plt.figure(figsize=figsize)
+    sns.boxplot(data=data)
+    plt.show()
+    if file_name:
+        plt.savefig(file_name)
+
+
+def hist(data, bins=30, figsize=(16, 12), file_name=None):
+    data.hist(bins=bins, figsize=figsize)
+    plt.show()
+    if file_name:
+        plt.savefig(file_name)
+
+
+def hist_by(
+        data,
+        by,
+        by_values,
+        bins=30,
+        figsize=(16, 12),
+        fontsize=20,
+        folder_name=None):
+
+    hist_groups = data.groupby(by).hist(
+        bins=bins,
+        figsize=figsize)
+    for value, hist in zip(by_values, hist_groups):
+        fig = hist[0, 0].get_figure()
+        plt.title('%s: %d' % (by, value), fontsize=fontsize)
+        fig.suptitle('%s: %d' % (by, value), fontsize=fontsize)
+        if folder_name:
+            fig.savefig('%shist-%s-%d.png' % (folder_name, by, value))
+        plt.close()
+
+
+def pairplot(data, file_name=None):
+    figure = sns.pairplot(data=data)
+    figure.savefig(file_name)
+    plt.show()
